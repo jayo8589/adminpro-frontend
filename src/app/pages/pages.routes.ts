3a1981ff75ccef3b26286458118f0012035786f4
jayo8589 <jayo8589@gmail.com>
2018-03-31 11:39:48 -0400

    Fin de la sección 16

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -10,6 +10,9 @@ import { RxjsComponent } from './rxjs/rxjs.component';
 import { LoginGuardGuard } from '../services/guards/login-guard.guard';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { HospitalesComponent } from './hospitales/hospitales.component';
+import { MedicosComponent } from './medicos/medicos.component';
+import { MedicoComponent } from './medicos/medico.component';
 
 
 
@@ -28,6 +31,9 @@ const pagesRoutes: Routes = [
             {path: 'perfil', component: ProfileComponent, data: {titulo: 'Perfil de Usuario'}},
             //Mantenimientos
             {path: 'usuarios', component: UsuariosComponent, data: {titulo: 'Mantenimiento de Usuarios'}},
+            {path: 'hospitales', component: HospitalesComponent, data: {titulo: 'Mantenimiento de Hospitales'}},
+            {path: 'medicos', component: MedicosComponent, data: {titulo: 'Mantenimiento de Médicos'}},
+            {path: 'medico/:id', component: MedicoComponent, data: {titulo: 'Actualizar Médico'}},
             {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
     }
